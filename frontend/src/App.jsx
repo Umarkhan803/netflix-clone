3908f2b90537461c29dd92029591fe667e5c2d61
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2024-10-22 12:41:35 +0530

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home/Home";
 import SignUp from "./page/SignUp";
 import LogIn from "./page/LogIn";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
       </Routes>
+      <Footer />
     </>
   );
 }
