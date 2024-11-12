eabbada0fc2ed79cf4bb9c0038857c5598f9bf1a
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2024-11-12 13:01:26 +0530

    backend env

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
-
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
@@ -15,7 +12,7 @@ const app = express();
 
 const PORT = ENV_VARS.PORT;
 
-app.use(express.json()); // will allow us to parse req.body
+app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("server is up"));
