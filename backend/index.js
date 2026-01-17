113a4d895dab49e64b11c0b5688e6967ce50b44f
Umarkhan803 <umar.khan@quantumquest.in>
2026-01-17 09:14:14 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,16 @@ import cors from "cors";
 const app = express();
 
 const PORT = ENV_VARS.PORT;
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
 app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("server is up"));
